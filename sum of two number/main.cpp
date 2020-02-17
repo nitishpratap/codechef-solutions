363f97ec3df6822177af03eed88391393439b1f2
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-18 01:48:08 +0530

    Add files via upload

---
+++ sum of two number/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,sum=0;
+        cin>>a>>b;
+        sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
