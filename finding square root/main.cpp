ea4be983d8ba589835dd55eb66ee5e7b6115408a
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-21 22:40:34 +0530

    Add files via upload

---
+++ finding square root/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int num,x;
+        cin>>num;
+        x=sqrt(num);
+        cout<<x<<endl;
+
+    }
+    return 0;
+}
