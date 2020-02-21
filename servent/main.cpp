4ab04a2adf5bbee2554437a132763c3c4c973dfa
nitishpratap <54417889+nitishpratap@users.noreply.github.com>
2020-02-21 22:54:22 +0530

    Add files via upload

---
+++ servent/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n<10)
+            cout<<"What an obedient servant you are!"<<endl;
+        else
+            cout<<"-1"<<endl;
+
+    }
+    return 0;
+}
